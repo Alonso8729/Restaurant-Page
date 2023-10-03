2f947e15507515e972a24aab0a7634112d34d224
Alonso8729 <alon8729@gmail.com>
2023-10-03 15:02:34 +0300

    Finished website.js module

--- src/website.js
+++ src/website.js
@@ -3,6 +3,65 @@ import loadContact from "./contact"
 import loadMenu from "./menu"
 
 
+function createHeader() {
+    const header = document.createElement('header');
+    //Create and append h1 element
+    const title = document.createElement('h1');
+    title.classList.add('title');
+    title.textContent = "Fresco";
+    header.appendChild(title);
+
+    //Create and append nav
+    const nav = document.createElement('nav');
+    header.appendChild(nav);
+
+    //Create event listeners and append nav's buttons
+    //HOME BUTTON
+    const homeButton = document.createElement('button');
+    homeButton.classList.add('nav-button');
+    homeButton.addEventListener('click', (e) => {
+        if (e.target.classList.contains("active")) return;
+        setToActive(homeButton);
+        loadHome();
+    })
+
+    //MENU BUTTON
+    const menuButton = document.createElement('button');
+    menuButton.classList.add('nav-button');
+    menuButton.addEventListener('click', (e) => {
+        if (e.target.classList.contains("active")) return;
+        setToActive(menuButton);
+        loadMenu();
+    })
+
+    //CONTACT BUTTON
+    const contactButton = document.createElement('button');
+    contactButton.classList.add('nav-button');
+    contactButton.addEventListener("click", (e) => {
+        if (e.target.classList.contains("active")) return;
+        setToActive(contactButton);
+        loadContact();
+    })
+
+
+
+    nav.appendChild(homeButton);
+    nav.appendChild(menuButton);
+    nav.appendChild(contactButton);
+
+    return header;
+
+}
+
+function setToActive(button) {
+    const buttons = document.querySelectorAll(".nav-button");
+    buttons.forEach((button) => {
+        if (button !== this) {
+            button.classList.remove('active')
+        }
+    })
+    button.classList.add("active");
+}
 
 
 function createMain() {
@@ -31,7 +90,7 @@ function loadWebsite() {
     content.appendChild(createFooter());
 
     //button that sets home active by adding class called active
-    //
+    setToActive(document.querySelector('.nav-button'))
 
     loadHome();
 }
