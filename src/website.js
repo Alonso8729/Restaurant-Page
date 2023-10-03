893fd592d1a50fa82c2dbe92ef9037fdc6704d4e
Alonso8729 <alon8729@gmail.com>
2023-10-03 14:41:15 +0300

    Finished createFooter function

--- src/website.js
+++ src/website.js
@@ -1,9 +1,23 @@
+import loadHome from "./home"
+import loadContact from "./contact"
+import loadMenu from "./menu"
 
 
 
 
 
 
+function createFooter() {
+    const footer = document.createElement('footer');
+    const paragraph = document.createElement('p');
+    const span = document.createElement('span');
+    span.textContent = "Alonso";
+    paragraph.textContent = "Built by";
+    paragraph.appendChild(span);
+    footer.appendChild(paragraph);
+
+    return footer;
+}
 
 function loadWebsite() {
     const content = document.querySelector("#content");
@@ -11,6 +25,8 @@ function loadWebsite() {
     content.appendChild(createMain());
     content.appendChild(createFooter());
 
+    //button that sets home active by adding class called active
+    //
 
     loadHome();
 }
